a6f4f79045692cf83982329a8581420b413ff335
Jefferson Mesa <mesajefer2@gmail.com>
2025-04-26 01:31:25 -0500

    fix some conections

--- src/main/java/eci/arcn/library/LibraryApplication.java
+++ src/main/java/eci/arcn/library/LibraryApplication.java
@@ -6,15 +6,15 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class LibraryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LibraryApplication.class, args);
-	}
+	//public static void main(String[] args) {
+	//	SpringApplication.run(LibraryApplication.class, args);
+	//}
 
-	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder.build();
-	}
+	//@Bean
+	//public RestTemplate restTemplate(RestTemplateBuilder builder) {
+	//	return builder.build();
+	//}
 }
